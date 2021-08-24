b5a919dfca2fa3e441e9befc8625b72ab377aff6
cnabilgit <46906074+cnabilgit@users.noreply.github.com>
2021-08-24 16:52:35 +0100

    ok

--- routes/web.php
+++ routes/web.php
@@ -6,16 +6,6 @@
      use App\Http\Controllers\UserController;
      use App\Http\Controllers\CategoryController;
 
-     /*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
 
 
      // Auth login
@@ -24,39 +14,29 @@
      Route::post('/login', [UserController::class, 'authenticate'])->name('auth.authenticate');
      Route::get('/register', [UserController::class, 'register'])->name('auth.register');
      Route::post('/register', [UserController::class, 'create'])->name('auth.create');
-
-
-
-     // Auth Register
-
      Route::get('/logout', [UserController::class, 'logout'])->name('auth.logout');
+
+
      Route::middleware(['auth'])->group(function () {
-          // Start group 
+          // Start group  with auth middlewear
 
 
           // HOME
-
-          Route::get('/{cat?}', [NoteController::class, 'index'])->name('home')->where('cat', '[0-9]+');;
-
+          Route::get('/{cat?}', [NoteController::class, 'index'])->name('home')->where('cat', '[0-9]+');
 
           // notes
-
           Route::get('/notes/create', [NoteController::class, 'create'])->name('notes.create');
           Route::post('/notes/store', [NoteController::class, 'store'])->name('notes.store');
           Route::post('/notes/destroy', [NoteController::class, 'destroy'])->name('notes.destroy');
 
 
           // Categories 
-
           Route::get('/categories/all', [CategoryController::class, 'index'])->name('categories');
-          Route::get('/categories/list', [CategoryController::class, 'getCategories'])
-               ->name('categories.list');
-          Route::post('/categories/store', [CategoryController::class, 'store'])
-               ->name('categories.store');
+          Route::get('/categories/list', [CategoryController::class, 'getCategories'])->name('categories.list');
+          Route::post('/categories/store', [CategoryController::class, 'store'])->name('categories.store');
 
 
           // Tags 
-
           Route::get('/tags', [TagController::class, 'index'])->name('tags');
           Route::post('/tags/store', [TagController::class, 'store'])->name('tags.store');
           Route::get('/tags/list', [TagController::class, 'getTags'])->name('tags.list');
@@ -65,9 +45,4 @@
           // settings 
           Route::get('/user/settings', [UserController::class, 'settings'])->name('auth.settings');
           Route::put('/settings/update', [UserController::class, 'updatesetting'])->name('updatesetting');
-
-
-
-
-          // end group
      });
